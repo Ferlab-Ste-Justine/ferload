bf8926cb23a18908e5cb911dd1b0da5e8b785246
Yann MARCOU <creativeyann17@gmail.com>
2021-06-28 15:42:10 -0400

    add config endpoint

---
+++ app/controllers/ConfigController.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import play.api.mvc._
+import play.api.{Configuration, Logging}
+import play.api.libs.json.Json.toJson
+
+import javax.inject._
+
+
+@Singleton
+class ConfigController @Inject()(val controllerComponents: ControllerComponents, val config: Configuration) extends BaseController with Logging {
+
+  private val keycloakUrl = config.get[String]("auth.url")
+  private val keycloakRealm = config.get[String]("auth.realm")
+  private val keycloakClientId = config.get[String]("auth.client-id")
+
+  def configuration(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val configuration = toJson(
+      Map(
+        "keycloak" ->
+          toJson(Map(
+            "url" -> toJson(keycloakUrl),
+            "realm" -> toJson(keycloakRealm),
+            "client-id" -> toJson(keycloakClientId)
+          )
+        ))
+      )
+
+    Ok(configuration)
+  }
+
+}
