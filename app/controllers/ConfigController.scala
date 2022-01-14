4737531d94f95096107e61af22e2b8640254052c
Yann MARCOU <creativeyann17@gmail.com>
2022-01-14 14:36:01 -0500

    feat: CLIN-698 - change auth logic

--- app/controllers/ConfigController.scala
+++ app/controllers/ConfigController.scala
@@ -13,6 +13,7 @@ class ConfigController @Inject()(val controllerComponents: ControllerComponents,
   private val keycloakUrl = config.get[String]("auth.url")
   private val keycloakRealm = config.get[String]("auth.realm")
   private val keycloakClientId = config.get[String]("auth.public-client-id")
+  private val keycloakAudience = config.get[String]("auth.audience")
 
   def configuration(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val configuration = toJson(
@@ -21,7 +22,8 @@ class ConfigController @Inject()(val controllerComponents: ControllerComponents,
           toJson(Map(
             "url" -> toJson(keycloakUrl),
             "realm" -> toJson(keycloakRealm),
-            "client-id" -> toJson(keycloakClientId)
+            "client-id" -> toJson(keycloakClientId),
+            "audience" -> toJson(keycloakAudience)
           )
         ))
       )
