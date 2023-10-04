95fe3d6bf4ef2abdc7a87da04928fef4953b2520
jecos <jcostanza@ferlab.bio>
2023-10-04 14:40:07 -0400

    WIP

--- src/test/scala/bio/ferlab/ferload/package.scala
+++ src/test/scala/bio/ferlab/ferload/package.scala
@@ -1,8 +1,8 @@
-package bio.ferlab.ferload
+package bio.ferlab
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 
-package object endpoints {
+package object ferload {
   extension[T] (t: IO[T]) def unwrap: T = t.unsafeRunSync()
 }
